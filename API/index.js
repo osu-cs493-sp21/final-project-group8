04430fca3c952493912f976c1c0198e3a4b73c36
Jia Hao <jiaha@oregonstate.edu>
2021-05-14 22:47:43 -0700

    Delete API directory

--- API/index.js
+++
@@ -1,6 +0,0 @@
-const router = module.exports = require('express').Router();
-
-router.use('/comments', require('./comments').router);
-router.use('/posts', require('./posts').router);
-router.use('/likes', require('./likes').router);
-router.use('/users', require('./users').router);
--- API/posts.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const mysqlPool = require("../lib/mysqlPool");
-const validation = require('../lib/validation');
-
-exports.router = router;
