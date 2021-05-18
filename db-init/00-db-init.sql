eebe9d60702c818a6c9845e7a22a980dfa137989
Hao Jia <jiaha@oregonstate.edu>
2021-05-17 22:04:31 -0700

    MySQL script

---
+++ db-init/00-db-init.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `users` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `userId` MEDIUMINT NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `userPassword` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`userId`)
+)
+
+
+CREATE TABLE `posts' (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `postId` MEDIUMINT NOT NULL,
+  `userId` MEDIUMINT NOT NULL,
+  `caption` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`postId`),
+  CONSTRAINT `fk_posts_users` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
+)
+
+
+CREATE TABLE `comments' (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `commentId` MEDIUMINT NOT NULL,
+  `userId` MEDIUMINT NOT NULL,
+  `postId` MEDIUMINT NOT NULL,
+  `review` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`commentId`),
+  CONSTRAINT `fk_comments_posts` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`)
+)
+
+
+CREATE TABLE `likes' (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `likeId` MEDIUMINT NOT NULL,
+  `userId` MEDIUMINT NOT NULL,
+  `postId` MEDIUMINT NOT NULL,
+  `like_post` BOOLEAN DEFAULT true,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`likeId`),
+  CONSTRAINT `fk_likes_posts` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`)
+)
